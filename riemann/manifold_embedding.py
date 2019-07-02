Fixed some issues with double precision

riemann/manifold_embedding.py
@@ -7,17 +7,13 @@ import numpy as np
 class ManifoldEmbedding(torch.nn.Embedding):
 
     def __init__(self, manifold: Manifold, num_embeddings, embedding_dim, padding_idx=None, max_norm=None,
-                 norm_type=2.0, scale_grad_by_freq=False, sparse=False, _weight=None, double_precision=True):
+                 norm_type=2.0, scale_grad_by_freq=False, sparse=False, _weight=None):
         super().__init__(num_embeddings, embedding_dim, padding_idx=padding_idx, max_norm=max_norm,
                  norm_type=norm_type, scale_grad_by_freq=scale_grad_by_freq, sparse=sparse, _weight=_weight)
 
         self.manifold = manifold
-        if double_precision:
-            data = self.weight.double()
-        else:
-            data = self.weight
 
-        self.weight = ManifoldParameter(data=data, manifold=manifold)
+        self.weight = ManifoldParameter(data=self.weight, manifold=manifold)
         self.weight.proj_()
 
 