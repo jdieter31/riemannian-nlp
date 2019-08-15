Fixed some bugs with numerical stability/gradient operations

riemann/graph_embedding_utils.py
@@ -11,6 +11,8 @@ from tqdm import tqdm
 from embed_save import Savable
 import numpy as np
 
+EPSILON = 1e-9
+
 def manifold_dist_loss(model: nn.Module, inputs: torch.Tensor,
     targets: torch.Tensor, manifold: RiemannianManifold):
     """
@@ -54,18 +56,21 @@ def manifold_dist_loss_kl(model: nn.Module, inputs: torch.Tensor, train_distance
     train_distrib = softmax(-train_distances, -1)
     return kl_div(manifold_dist_distrib, train_distrib, reduction="batchmean")
 
-def manifold_dist_loss_relu_sum(model: nn.Module, inputs: torch.Tensor, train_distances: torch.Tensor, manifold: RiemannianManifold):
+def manifold_dist_loss_relu_sum(model: nn.Module, inputs: torch.Tensor, train_distances: torch.Tensor, manifold: RiemannianManifold, margin=0.01, discount_factor=0.9):
     input_embeddings = model(inputs)
+
     sample_vertices = input_embeddings.narrow(1, 1, input_embeddings.size(1)-1)
     main_vertices = input_embeddings.narrow(1, 0, 1).expand_as(sample_vertices)
     manifold_dists = manifold.dist(main_vertices, sample_vertices)
+
     sorted_indices = train_distances.argsort(dim=-1)
     manifold_dists_sorted = torch.gather(manifold_dists, -1, sorted_indices)
+    manifold_dists_sorted.add_(EPSILON).log_()
     diff_matrix_shape = [manifold_dists.size()[0], manifold_dists.size()[1], manifold_dists.size()[1]]
     row_expanded = manifold_dists_sorted.unsqueeze(2).expand(*diff_matrix_shape)
     column_expanded = manifold_dists_sorted.unsqueeze(1).expand(*diff_matrix_shape)
-    margin = 1
     diff_matrix = row_expanded - column_expanded + margin
+
     train_dists_sorted = torch.gather(train_distances, -1, sorted_indices)
     train_row_expanded = train_dists_sorted.unsqueeze(2).expand(*diff_matrix_shape)
     train_column_expanded = train_dists_sorted.unsqueeze(1).expand(*diff_matrix_shape)
@@ -73,13 +78,11 @@ def manifold_dist_loss_relu_sum(model: nn.Module, inputs: torch.Tensor, train_di
     masked_diff_matrix = torch.where(diff_matrix_train == 0, diff_matrix_train, diff_matrix)
     masked_diff_matrix.triu_()
     relu_(masked_diff_matrix)
-    masked_diff_matrix = masked_diff_matrix.sum(-1)
-    masked_diff_matrix += 1
-    masked_diff_matrix.log_()
-    order_scale = torch.arange(1, masked_diff_matrix.size()[1] + 1, device=masked_diff_matrix.device, dtype=masked_diff_matrix.dtype)
-    order_scale.pow_(1.5)
+    masked_diff_matrix = masked_diff_matrix.mean(-1)
+    order_scale = torch.arange(0, masked_diff_matrix.size()[1], device=masked_diff_matrix.device, dtype=masked_diff_matrix.dtype)
+    order_scale = (torch.ones_like(order_scale) * discount_factor).pow(order_scale)
     order_scale = order_scale.unsqueeze_(0).expand_as(masked_diff_matrix) 
-    masked_diff_matrix /= order_scale
+    masked_diff_matrix *= order_scale
     loss = masked_diff_matrix.sum(-1).mean()
     return loss
 

riemann/manifolds/sphere.py
@@ -73,7 +73,7 @@ class SphericalManifold(RiemannianManifold):
 
     def log(self, x, y):
         u = y - x
-        u.sub_((x * u).sum(dim=-1, keepdim=True) * x)
+        u = u - (x * u).sum(dim=-1, keepdim=True) * x
         dist = self.dist(x, y, keepdim=True)
         norm_u = u.norm(dim=-1, keepdim=True)
         cond = norm_u > EPSILON
@@ -81,8 +81,6 @@ class SphericalManifold(RiemannianManifold):
 
     def dist(self, x, y, keepdim=False):
         inner = (x * y).sum(-1, keepdim=keepdim)
-        # Scale slightly down to keep things differentiable and use Euclidean distance when
-        # it's an approriate approximation
         inner = inner.clamp(-1, 1)
         return acos(inner)
 

riemann/rsgd_multithread.py
@@ -5,17 +5,22 @@ from torch.optim.optimizer import Optimizer, required
 import torch
 from manifold_tensors import ManifoldParameter
 from manifolds import EuclideanManifold
+from logging_thread import write_log
 
 class RiemannianSGD(Optimizer):
 
     def __init__(
             self,
             params,
-            lr=0.001
+            lr=0.001,
+            clip_grads=True,
+            clip_val=10
     ):
         defaults = {
             'lr': lr,
         }
+        self.clip_grads=clip_grads
+        self.clip_val=clip_val
         super(RiemannianSGD, self).__init__(params, defaults)
 
     def step(self, **kwargs):
@@ -41,11 +46,19 @@ class RiemannianSGD(Optimizer):
                         indices = d_p._indices()[0]
 
                         manifold.rgrad_(p[indices], d_p._values())
+                        if self.clip_grads:
+                            if d_p._values().max() > self.clip_val or d_p._values().min() < -self.clip_val:
+                                write_log(f"Warning -- riemannian-gradients were clipped on {manifold} with max_val {d_p._values().abs().max()}")
+
+                            d_p._values().clamp(-self.clip_val/lr, self.clip_val/lr)
                         manifold.retr_(p, d_p._values() * (-lr), indices=indices)
                     else:
                         d_p = p.grad.data
                         manifold.rgrad_(p.data, d_p)
+                        if self.clip_grads:
+                            if d_p.max() > self.clip_val or d_p.min() < -self.clip_val:
+                                write_log(f"Warning -- gradients were clipped on {manifold} with max_val {d_p.abs().max()}")
+                            d_p = d_p.clamp(-self.clip_val, self.clip_val)
                         manifold.retr_(p.data, d_p * (-lr))
-                
 
         return loss

riemann/train.py
@@ -15,6 +15,7 @@ def train(
         manifold,
         data,
         optimizer,
+        loss_params,
         n_epochs,
         eval_every,
         lr_scheduler,
@@ -33,21 +34,22 @@ def train(
         data_iterator = tqdm(data) if thread_number == 0 else data
 
         for batch in data_iterator:
-            if data.sample_data == "targets":
-                inputs, targets = batch
-                inputs = inputs.to(device)
-                targets = targets.to(device)
-                optimizer.zero_grad()
-                loss = manifold_dist_loss(model, inputs, targets, manifold)
-            elif data.sample_data == "graph_dist":
-                inputs, graph_dists = batch
-                inputs = inputs.to(device)
-                graph_dists = graph_dists.to(device)
-                optimizer.zero_grad()
-                loss = manifold_dist_loss_relu_sum(model, inputs, graph_dists, manifold)
+            with torch.autograd.detect_anomaly():
+                if data.sample_data == "targets":
+                    inputs, targets = batch
+                    inputs = inputs.to(device)
+                    targets = targets.to(device)
+                    optimizer.zero_grad()
+                    loss = manifold_dist_loss(model, inputs, targets, manifold)
+                elif data.sample_data == "graph_dist":
+                    inputs, graph_dists = batch
+                    inputs = inputs.to(device)
+                    graph_dists = graph_dists.to(device)
+                    optimizer.zero_grad()
+                    loss = manifold_dist_loss_relu_sum(model, inputs, graph_dists, manifold, **loss_params)
 
-            loss.backward()
-            optimizer.step()
+                loss.backward()
+                optimizer.step()
             batch_losses.append(loss.cpu().detach().numpy())
             elapsed = timeit.default_timer() - t_start
 